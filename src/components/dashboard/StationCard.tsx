9c2659435e6a19f4a210d5a589fab8f60e6c338a
ppirch <pakin.pirch@gmail.com>
2022-01-11 10:32:35 +0700

    Center Topic

--- src/components/dashboard/StationCard.tsx
+++ src/components/dashboard/StationCard.tsx
@@ -8,18 +8,21 @@ type StationProps = {
 const StationCard: FC<StationProps> = ({ station }) => {
   return (
     <Box>
-      <Paper
-        sx={{
-          backgroundColor: "#383737",
-          borderRadius: 2,
-          width: 200,
-          paddingLeft: 2,
-        }}
-      >
-        <Typography variant="h6" color="white">
-          สถานี: {station.name}
-        </Typography>
-      </Paper>
+      <Box display="flex" alignItems="center" justifyContent="center">
+        <Paper
+          sx={{
+            backgroundColor: "#383737",
+            borderRadius: 2,
+            width: 200,
+            paddingLeft: 2,
+          }}
+        >
+          <Typography variant="h6" color="white">
+            สถานี: {station.name}
+          </Typography>
+        </Paper>
+      </Box>
+
       <Box sx={{ marginTop: 2 }}>
         <Typography variant="h6">อุณหภูมิ: {station.data.temp} °C</Typography>
         <Typography variant="h6">ความชื้น: {station.data.humid} %</Typography>
