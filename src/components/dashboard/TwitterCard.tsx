2251e46b99b7b5de93cbb6246b556dbb6a8e41ac
ppirch <pakin.pirch@gmail.com>
2022-01-11 20:23:07 +0700

    Add twitter stat

--- src/components/dashboard/TwitterCard.tsx
+++ src/components/dashboard/TwitterCard.tsx
@@ -1,4 +1,4 @@
-import { Twitter } from '@mui/icons-material'
+import { AutorenewRounded, ChatBubbleRounded, FavoriteRounded, Twitter } from '@mui/icons-material'
 import { Box, Icon, Stack, Typography } from '@mui/material'
 import { FC } from 'react'
 import { ITwitter } from '../../interfaces/mqtt'
@@ -23,6 +23,20 @@ const TwitterCard: FC<TwitterProps> = ({ twitter }) => {
       </Box>
       <Box sx={{ padding: 2 }}>
         <Typography variant='h6'>{twitter.data.text}</Typography>
+        <Stack direction='row' justifyContent='space-evenly' alignItems='center' spacing={2}>
+          <Box display='flex'>
+            <ChatBubbleRounded sx={{ fontSize: 24, color: '#383737', marginRight: 1 }} />
+            <Typography sx={{ fontSize: 16 }}>{twitter.reply_count}</Typography>
+          </Box>
+          <Box display='flex'>
+            <AutorenewRounded sx={{ fontSize: 24, color: '#383737', marginRight: 1 }} />
+            <Typography sx={{ fontSize: 16 }}>{twitter.retweet_count}</Typography>
+          </Box>
+          <Box display='flex'>
+            <FavoriteRounded sx={{ fontSize: 24, color: '#383737', marginRight: 1 }} />
+            <Typography sx={{ fontSize: 16 }}>{twitter.favorite_count}</Typography>
+          </Box>
+        </Stack>
       </Box>
     </Stack>
   )
